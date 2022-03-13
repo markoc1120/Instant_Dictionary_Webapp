8605038716fa7cda2177ce7e4a89e68cbe013b6f
markoc1120 <markoc1120@gmail.com>
2022-03-13 15:57:00 +0100

    Create home and about pages

---
+++ webapp/home.py
@@ -0,0 +1,21 @@
+import justpy as jp
+
+
+class Home:
+    path = '/'
+
+    def serve(self):
+        wp = jp.QuasarPage(tailwind=True)
+        div = jp.Div(a=wp, classes='bg-gray-200 h-screen')
+        jp.Div(a=div, text='This is the Home page!', classes='text-4xl m-2')
+        jp.Div(a=div, text="""
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent imperdiet a turpis in suscipit. Mauris 
+                pulvinar massa nisi, quis malesuada augue molestie nec. Donec consectetur hendrerit mollis. Sed ut magna et 
+                lectus sollicitudin pharetra a commodo lectus. Quisque tortor arcu, sodales id urna vitae, rhoncus congue 
+                augue. Suspendisse luctus congue purus ac aliquam. Etiam vestibulum, leo nec fringilla ultricies, urna metus 
+                luctus ante, nec placerat mauris lectus id lorem. Sed in bibendum ligula, a ornare leo. In hac habitasse platea 
+                dictumst. Cras porta, tellus sit amet iaculis vestibulum, orci dui semper turpis, eget consequat metus arcu 
+                vitae nisl. In mattis nibh vitae dictum volutpat. In porta tellus nec luctus mattis. Duis eleifend magna ut 
+                vulputate elementum. Pellentesque ex orci, pulvinar vel condimentum ut, tincidunt non nulla.
+                """, classes='text-lg')
+        return wp
