087a6b2d1ffb3db4b7adb14ccc57705ce445f40b
markoc1120 <markoc1120@gmail.com>
2022-03-13 21:37:22 +0100

    Make routing automatic

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
+import inspect
 import justpy as jp
-from webapp.about import About
-from webapp.home import Home
-from webapp.dictionary import Dictionary
+from webapp import page
 
+imports = list(globals().values())
 
-jp.Route(Home.path, Home.serve)
-jp.Route(About.path, About.serve)
-jp.Route(Dictionary.path, Dictionary.serve)
-
+for obj in imports:
+    if inspect.isclass(obj):
+        if issubclass(obj, page.Page) and obj is not page.Page:
+            jp.Route(obj.path, obj.serve)
 
 jp.justpy(port=8002)
